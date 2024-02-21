a2af5795302d52c9bb680c053682f056b67d5471
JohnM L <lottjohnmichael@yahoo.com>
2024-02-20 19:03:59 -0800

    update 2

--- festivalProjections.java
+++ festivalProjections.java
@@ -13,7 +13,7 @@
 
     public static void main(String[] args){
         int choice, itemCount;
-        double admission, hotelCost = 300.0, bnbCost = 210.0, 
+        double admission = 600.0, hotelCost = 300.0, bnbCost = 210.0, 
         campCost= 90.0, foodCost = 180, carParkCost = 90.0,
         total = 0;
         String msgError = "Error! Try again.",
@@ -22,12 +22,13 @@
          Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Hello Welcome to Festival Projections!\n");
-        System.out.println("Please make a choice from the menu options to get started.\n");
-        System.out.println("__________________________________________________________\n");
+        System.out.println("Please make a choice from the menu options to get started.");
+        System.out.println("__________________________________________________________");
         System.out.println("1. Make Festival Cost Projection");
         System.out.println("2. Access Saved Projections");
         System.out.println("3. Change Presets");
         System.out.println("4. Exit");
+        System.out.print("Selection: ");
         choice = keyboard.nextInt();
 
         while(choice < 1 || choice > 4){
@@ -38,13 +39,17 @@
         while(total == 0){
             switch(choice){
                 case 1:{
-                    
+                    System.out.println("The total cost projection for " + festName + " is " + total);
+                    System.out.printf("The total cost projection for %s is ");
+                    total = campCost + foodCost + carParkCost + admission + hotelCost;
                     }break;
                 case 2:{
-                    
+                    System.out.println("None saved");
+                    total = -1;
                     }break;
                 case 3:{
-                    
+                    System.out.println("Unable to change presets currently. :(");
+                    total = -1;
                     }break;
                 default:
                     total = -1;
